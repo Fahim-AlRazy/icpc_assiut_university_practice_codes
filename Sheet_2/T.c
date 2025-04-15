caf9cecd9428f9fee9692845b575e4fcfc0bd61a
Fahim al razy <fahim.al.razy@g.bracu.ac.bd>
2025-04-15 13:49:53 +0600

    some edit

--- Sheet_2/T.c
+++ Sheet_2/T.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 int main(void){
-    int N,k=0;
+    int N;
     scanf("%d", &N);
     for(int i=1; i<=N; i++){
-        for(int j=0;j<i+k;j++){
+        for (int j = 0; j < N - i; j++) {
+            printf(" ");
+        }
+        for(int j=0;j<2*i-1;j++){
             printf("*");
         }
-        k++;
         printf("\n");
     }
     return 0;
--- Sheet_2/W.c
+++ Sheet_2/W.c
@@ -3,12 +3,18 @@ int main(void){
     int N,k=0;
     scanf("%d",&N);
     for(int i=1; i<=N; i++){
+        for (int j = 0; j < N - i; j++) {
+            printf(" ");
+        }
         for(int j=0;j<(2 * i - 1);j++){
             printf("*");
         }
         printf("\n");
     }
     for(int i=N; i>0; i--){
+        for (int j = N - i; j > 0; j--) {
+            printf(" ");
+        }
         for(int j=0;j<(2 * i - 1);j++){
             printf("*");
         }
