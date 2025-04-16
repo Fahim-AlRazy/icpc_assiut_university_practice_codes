77c22c9821ec0acfbd1db397a064868866127fe6
Fahim al razy <fahim.al.razy@g.bracu.ac.bd>
2025-04-16 19:32:52 +0600

    first commit

--- Sheet_3/A.c
+++ Sheet_3/A.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 int main(void){
-    
+    int N,sum=0;
+    scanf("%d", &N);
+    int A[N];
+    for(int i=0; i<N; i++){
+        scanf("%lld", &A[i]);
+        if(A[i] < 0){
+            A[i] = -A[i];
+        }
+        sum += A[i];
+    }
+    printf("%d", sum);
     return 0;
 }
