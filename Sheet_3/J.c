f4e4b6b1fd3f0006f8cb87a1a1c13a05d3a38407
Fahim al razy <fahim.al.razy@g.bracu.ac.bd>
2025-04-19 12:51:32 +0600

    hacker rank start

--- Sheet_3/J.c
+++ Sheet_3/J.c
@@ -1,5 +1,27 @@
 #include<stdio.h>
 int main(void){
-    
+    int N,min=0,count=0;
+    scanf("%d", &N);
+    int A[N];
+    for ( int i = 0; i < N; i++){
+        scanf("%d", &A[i]);
+    }
+    min=A[0];
+    for(int i = 1; i < N; i++){
+        if(A[i]<min){
+            min=A[i];
+        }
+    }
+for(int i = 0; i < N; i++){
+    if(A[i]==min){
+        count++;
+    }
+}
+if (count%2!=0){
+    printf("Lucky\n");
+}
+else{
+    printf("Unlucky\n");
+}
     return 0;
 }
